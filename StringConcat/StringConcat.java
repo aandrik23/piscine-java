0dce99de344428f7bb94d05c266eac06ab8e5305
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-19 00:40:23 +0300

    concat

---
+++ StringConcat/StringConcat.java
@@ -0,0 +1,13 @@
+public class StringConcat {
+    public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return "";
+        } else if (s1 == null) {
+            return s2;
+        } else if (s2 == null) {
+            return s1;
+        } else {
+            return s1 + s2;
+        }
+    }
+}
