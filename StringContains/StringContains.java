3f93c53e2822a9a93d9dc5285ba5794cf69b9ee7
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-19 00:54:47 +0300

    contains

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,6 +1,6 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
-        if (s == null) {
+        if (s == null || subString == null) {
             return false;
         } else {
             return s.contains(subString);
