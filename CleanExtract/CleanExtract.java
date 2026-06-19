6ad9856c6fcd92cc238ec48a85cd4429504126c1
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-19 23:53:04 +0300

    clean

---
+++ CleanExtract/CleanExtract.java
@@ -0,0 +1,21 @@
+public class CleanExtract {
+    public static String extract(String s){
+        String[] parts = s.split("\\|");
+        String result = "";
+        for (int i=0; i <parts.length; i++) {
+            int firstDot = parts[i].indexOf(".");
+            int lastDot = parts[i].lastIndexOf(".");
+
+            String extracted;
+            if (firstDot == -1 && lastDot == -1) {
+                extracted = parts[i].trim();
+            } else {
+                extracted = parts[i].substring(firstDot + 1, lastDot).trim();
+            }
+            if (!extracted.isEmpty()) {
+                result += extracted + " ";
+            }
+        }
+        return result.trim();
+    }
+}
