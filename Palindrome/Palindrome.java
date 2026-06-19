f6660585519e8aa5d493c3a03948a198698cca76
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-20 01:28:06 +0300

    palindrome3

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,7 +1,13 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
         String reversed = "";
+
+        if (s == null) {
+            return false;
+        }
+
         s = s.toLowerCase();
+
         for (int i = s.length() - 1; i >= 0 ; i--){
             reversed += s.charAt(i);
         }
