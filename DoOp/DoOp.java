5df9f384a4304f7b0f2b7128cb476758638a0e5a
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-20 16:32:07 +0300

    doOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,34 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        int left = Integer.parseInt(args[0]);
+        String op = args[1];
+        int right = Integer.parseInt(args[2]);
+
+        switch (op) {
+            case "+":
+                return String.valueOf(left + right);
+
+            case "-":
+                return String.valueOf(left - right);
+
+            case "*":
+                return String.valueOf(left * right);
+
+            case "/":
+                if (right == 0) {
+                    return "Error";
+                } else {
+                    return String.valueOf(left / right);
+                }
+            case "%":
+                if (right == 0) {
+                    return "Error";
+                } else {
+                    return String.valueOf(left % right);
+                }
+
+            default:
+                return "Error";
+        }
+    }
+}
