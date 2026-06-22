d71845124dc2f47550b0237ed87d11310339d436
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-23 01:12:15 +0300

    celestial object

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,17 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+}
