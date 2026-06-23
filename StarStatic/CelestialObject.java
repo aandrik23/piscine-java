eed17e6f6541cf8fe2fa27a3cc5e737aeefca0bb
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-23 21:42:32 +0300

    math

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -1,8 +1,51 @@
 public class CelestialObject {
-    public static final double KM_IN_ONE_AU = 150000000;
     private double x;
     private double y;
     private double z;
+    private String name;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public static double getDistanceBetween(
             CelestialObject obj1,
@@ -15,6 +58,6 @@ public class CelestialObject {
 
         return Math.sqrt(sum);
     }
-}
 
+}
 
