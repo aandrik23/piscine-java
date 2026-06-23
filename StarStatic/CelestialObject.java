1b358c3e663b105b77fc78f01342adc8b0eb9faa
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-23 21:30:52 +0300

    maths

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -3,18 +3,18 @@ public class CelestialObject {
     private double x;
     private double y;
     private double z;
+
     public static double getDistanceBetween(
             CelestialObject obj1,
-            CelestialObject obj2)
-        {
-            double dx = obj2.getX() - obj1.getX();
-            double dy = obj2.getY() - obj1.getY();
-            double dz = obj2.getZ() - obj1.getZ();
+            CelestialObject obj2) {
+        double dx = obj2.getX() - obj1.getX();
+        double dy = obj2.getY() - obj1.getY();
+        double dz = obj2.getZ() - obj1.getZ();
 
-            double sum = (Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2));
+        double sum = (Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2));
 
-            return Math.sqrt(sum);
-        }
-
+        return Math.sqrt(sum);
+    }
+}
 
 
