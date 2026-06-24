e0117dc75f4078c1d9ca165048b5dca52d0457a2
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-25 00:34:45 +0300

    galaxy

--- StarGalaxy/CelestialObject.java
+++ StarGalaxy/CelestialObject.java
@@ -1,70 +1,53 @@
 import java.util.Objects;
+
 public class CelestialObject {
     public static final double KM_IN_ONE_AU = 150000000;
+
     private double x;
     private double y;
     private double z;
     private String name;
+    private int mass;
 
     public CelestialObject() {
         this.name = "Soleil";
     }
 
     public CelestialObject(String name, double x, double y, double z) {
+        this(name, x, y, z, 0);
+    }
+
+    public CelestialObject(String name, double x, double y, double z, int mass) {
+        this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
-        this.name = name;
+        this.mass = mass;
     }
 
-    public double getX() {
-        return x;
-    }
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getZ() { return z; }
+    public String getName() { return name; }
+    public int getMass() { return mass; }
 
-    public double getY() {
-        return y;
-    }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setZ(double z) { this.z = z; }
+    public void setName(String name) { this.name = name; }
+    public void setMass(int mass) { this.mass = mass; }
 
-    public double getZ() {
-        return z;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static double getDistanceBetween(
-            CelestialObject obj1,
-            CelestialObject obj2) {
+    public static double getDistanceBetween(CelestialObject obj1, CelestialObject obj2) {
         double dx = obj2.getX() - obj1.getX();
         double dy = obj2.getY() - obj1.getY();
         double dz = obj2.getZ() - obj1.getZ();
 
-        double sum = (Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2));
-
-        return Math.sqrt(sum);
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2));
     }
 
     public static double getDistanceBetweenInKm(CelestialObject obj1, CelestialObject obj2) {
         return getDistanceBetween(obj1, obj2) * KM_IN_ONE_AU;
     }
-
 
     @Override
     public String toString() {
@@ -73,26 +56,20 @@ public class CelestialObject {
 
     @Override
     public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (object == null) {
-            return false;
-        }
-
-        if (!(object instanceof CelestialObject)) {
-            return false;
-        }
+        if (this == object) return true;
+        if (!(object instanceof CelestialObject)) return false;
 
         CelestialObject other = (CelestialObject) object;
 
-        return x == other.x && y == other.y && z == other.z && name.equals(other.name);
+        return x == other.x
+                && y == other.y
+                && z == other.z
+                && mass == other.mass
+                && name.equals(other.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, x, y, z);
+        return Objects.hash(name, x, y, z, mass);
     }
 }
-
--- StarGalaxy/Star.java
+++ StarGalaxy/Star.java
@@ -1,28 +1,26 @@
 import java.util.Objects;
 
-public class Star extends CelestialObject{
+public class Star extends CelestialObject {
     private double magnitude;
+
+    public Star() {
+        super();
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        this(name, x, y, z, magnitude, 0);
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude, int mass) {
+        super(name, x, y, z, mass);
+        this.magnitude = magnitude;
+    }
 
     public double getMagnitude() {
         return magnitude;
     }
 
     public void setMagnitude(double magnitude) {
-        this.magnitude = magnitude;
-    }
-
-    public Star() {
-        super();
-    }
-
-    public Star(
-            String name,
-            double x,
-            double y,
-            double z,
-            double magnitude
-    ) {
-        super(name, x, y, z);
         this.magnitude = magnitude;
     }
 
@@ -33,28 +31,15 @@ public class Star extends CelestialObject{
 
     @Override
     public boolean equals(Object object) {
-        if (!super.equals(object)) {
-            return false;
-        }
-
-        if (!(object instanceof Star)) {
-            return false;
-        }
+        if (!super.equals(object)) return false;
+        if (!(object instanceof Star)) return false;
 
         Star other = (Star) object;
-
         return magnitude == other.magnitude;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(
-                getName(),
-                getX(),
-                getY(),
-                getZ(),
-                magnitude
-        );
+        return Objects.hash(getName(), getX(), getY(), getZ(), getMass(), magnitude);
     }
-
 }
