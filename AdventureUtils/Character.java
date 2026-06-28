8a1dd2de6be69115a21a98d062f744f7cba5d110
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-29 02:13:00 +0300

    Utils

---
+++ AdventureUtils/Character.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+    private static List<Character> allCharacters = new ArrayList<>();
+    private final String name;
+    private final int maxHealth;
+    private int currentHealth;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+
+        allCharacters.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (currentHealth == 0) {
+            return String.format("%s : KO", name);
+        }
+
+        return String.format("%s : %d/%d", name, currentHealth, maxHealth);
+    }
+
+    public void takeDamage(int damage) {
+        currentHealth -= damage;
+        
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        }
+    }
+
+    public void attack(Character character) {
+        character.takeDamage(9);
+    }
+
+    public static String printStatus() {
+        String result = "------------------------------------------\n";
+        if (allCharacters.isEmpty()) {
+            result += "Nobody's fighting right now !\n";
+            result += "------------------------------------------";
+            return result;
+        }
+        result += "Characters currently fighting : \n";
+
+        for (Character character : allCharacters) {
+            result += " - " + character.toString() + "\n";
+        }
+        result += "------------------------------------------";
+        return result;
+    }
+
+    public static Character fight(Character first, Character second) {
+        while (first.getCurrentHealth() > 0 && second.getCurrentHealth() > 0) {
+
+            first.attack(second);
+
+            if (second.getCurrentHealth() == 0) {
+                return first;
+            }
+
+            second.attack(first);
+
+            if (first.getCurrentHealth() == 0) {
+                return second;
+            }
+        }
+
+        return null;
+    }
+}
