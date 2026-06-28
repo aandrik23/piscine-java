3c69a59e1721c92ce750d3b2bc5a2fa36cf45e1d
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-06-29 02:17:26 +0300

    utils4

--- AdventureUtils/Character.java
+++ AdventureUtils/Character.java
@@ -52,7 +52,7 @@ public class Character {
         if (allCharacters.isEmpty()) {
             return "------------------------------------------\n"
                     + "Nobody's fighting right now !\n"
-                    + "------------------------------------------\n";
+                    + "------------------------------------------";
         }
 
         String result = "------------------------------------------\n"
@@ -62,7 +62,7 @@ public class Character {
             result += " - " + character.toString() + "\n";
         }
 
-        result += "------------------------------------------\n";
+        result += "------------------------------------------";
         return result;
     }
 
