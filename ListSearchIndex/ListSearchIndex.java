126d648691c191245c8844dd3b05c4879dff8dc0
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-02 23:07:28 +0300

    search3

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -1,36 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class ListSearchIndex {
-    public static Integer findFirstIndex(List<Integer> list, Integer value) {
-        int index = list.indexOf(value);
-
-        if (index == -1) {
-            return null;
-        }
-
-        return index;
-    }
-
-    public static Integer findLastIndex(List<Integer> list , Integer value) {
-        int index = list.lastIndexOf(value);
-
-        if (index == -1) {
-            return null;
-        }
-        return index;
-    }
-
-    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
-        List<Integer> indexes = new ArrayList<>(); {
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).equals(value)) {
-                    indexes.add(i);
-                }
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null) return null;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i).equals(value)) {
+                return i;
             }
-            return indexes;
         }
+        return null;
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null) return null;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                return i;
+            }
+        }
+        return null;
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (list == null) return new ArrayList<>();
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
     }
 }
-
-
