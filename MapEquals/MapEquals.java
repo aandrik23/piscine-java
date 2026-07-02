8d8e554d15409dfba4cd29819446fe1254a5a80c
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-02 23:32:16 +0300

    map equals

--- MapEquals/MapEquals.java
+++ MapEquals/MapEquals.java
@@ -2,7 +2,7 @@ import java.util.Map;
 
 public class MapEquals {
     public static boolean areMapsEqual(Map<String, Integer> map1, Map<String, Integer> map2) {
-        if (map1 == null %% map2 == null) {
+        if (map1 == null && map2 == null) {
             return true;
         }
 
