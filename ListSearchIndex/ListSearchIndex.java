c6418ed540d1ea79c4e416e6a8fbc11387c41c75
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-02 22:58:18 +0300

    search

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,33 @@
+public class ListSearchIndex {
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        int index = List.indexOf(value);
+
+        if (index == -1) {
+            return null;
+        }
+
+        return index;
+    }
+
+    public static Integer findLastIndex(List<Integer> list , Integer value) {
+        int index = list.lastIndexOf(value);
+
+        if (index == -1) {
+            return null;
+        }
+        return index;
+    }
+
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> indexes = new ArrayList<>() {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(value)) {
+                    indexes.add(i);
+                }
+            }
+            return indexes;
+        }
+    }
+}
+
+
