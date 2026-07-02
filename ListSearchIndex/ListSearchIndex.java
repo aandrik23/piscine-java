106047f7dd357433f14f7a18624a29f7ee12f655
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-02 23:10:30 +0300

    search4

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
         if (list == null) return null;
