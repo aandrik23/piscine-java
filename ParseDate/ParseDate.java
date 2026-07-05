00edf047f85a3f2bc8116ac6408593be8b4553c4
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-05 20:17:59 +0300

    parse date

---
+++ ParseDate/ParseDate.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class ParseDate {
+    public static LocalDateTime parseIsoFormat(String stringDate) {
+        return LocalDateTime.parse(stringDate);
+    }
+
+    public static LocalDate parseFullTextFormat(String stringDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH);
+        return LocalDate.parse(stringDate, formatter);
+    }
+
+    public static LocalTime parseTimeFormat(String stringDate) {
+        String[] parts = stringDate.split(" ");
+        int hour = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[5]);
+        int seconds = Integer.parseInt(parts[8]);
+
+        if (stringDate.contains("du soir")) {
+            hour += 12;
+        }
+
+        return LocalTime.of(hour, minutes, seconds);
+    }
+}
