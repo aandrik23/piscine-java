7567068be5d36199430c3313e6e352ad7074afa8
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-06 00:44:55 +0300

    format date

---
+++ FormatDate/FormatDate.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatDate {
+
+    public static String formatToFullText(LocalDateTime dateTime) {
+
+        if (dateTime == null) {
+            return null;
+        }
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern(
+                        "'Le' d MMMM 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'",
+                        Locale.FRENCH);
+
+        return dateTime.format(formatter);
+    }
+
+    public static String formatSimple(LocalDate date) {
+
+        if (date == null) {
+            return null;
+        }
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
+
+        return date.format(formatter);
+    }
+
+    public static String formatIso(LocalTime time) {
+
+        if (time == null) {
+            return null;
+        }
+
+        return time.toString();
+    }
+}
