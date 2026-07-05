1913083682cbb4333c53781366a1391b1c98b1e1
aandrik23 <andreasrafailandrikopoulos@gmail.com>
2026-07-06 01:15:51 +0300

    regex replace

---
+++ RegexReplace/RegexReplace.java
@@ -0,0 +1,53 @@
+public class RegexReplace {
+    public static String removeUnits(String s) {
+        if (s == null) {
+            return null;
+        }
+        return s.replaceAll("(?<=\\d)(cm|€)(?=\\s|$)", "");
+    }
+
+    public static String obfuscateEmail(String s) {
+
+        if (s == null) {
+            return null;
+        }
+
+        String[] email = s.split("@");
+
+        if (email.length != 2) {
+            return s;
+        }
+
+        String username = email[0];
+        String domain = email[1];
+
+        if (username.contains(".")) {
+            username = username.replaceFirst("\\..*", ".***");
+        } else if (username.contains("-")) {
+            username = username.replaceFirst("-.*", "-***");
+        } else if (username.contains("_")) {
+            username = username.replaceFirst("_.*", "_***");
+        } else if (username.length() > 3) {
+            username = username.substring(0, 3) + "***";
+        }
+
+        String[] parts = domain.split("\\.");
+
+        if (parts.length == 2) {
+
+            String tld = parts[1];
+
+            if (tld.equals("com") || tld.equals("org") || tld.equals("net")) {
+                domain = "*******." + tld;
+            } else {
+                domain = "*******." + "*".repeat(tld.length());
+            }
+        } else if (parts.length == 3) {
+
+            domain = "*******." + parts[1] + "." + "*".repeat(parts[2].length());
+        }
+        return username + "@" + domain;
+
+    }
+
+}
